1bcd41a7c4bd9d494f37603eca7895049ff473e9
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-02-01 19:11:40 +0100

    update site meta tags for social media

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "lmssieh - portfolio";
+export const SITE_DESCRIPTION =
+  "I am a software developer with a passion for designing digital products that offer exceptional user experiences.";
