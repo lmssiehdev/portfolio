78bc9d3e1dc1b8e5bf41ebb7cbbdd8c14213b1e9
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2026-01-10 20:47:33 +0100

    update metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Lmssieh.';
+export const SITE_DESCRIPTION = 'Full stack TypeScript developer.';
